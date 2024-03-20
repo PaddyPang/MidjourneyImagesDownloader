6281d667940b1bb9fc29b9aef19431593fcd74a4
Paddy <pangyj@sharkpark.cn>
2024-03-20 12:08:42 +0800

    updated

--- content.js
+++ content.js
@@ -1,1 +1,61 @@
-function initDownloadAllBtn(t) { if (!t.querySelector("button.download-mj-all")) { const e = document.createElement("button"); e.classList.add("component-ifCTxY"), e.classList.add("button-ejjZWC"), e.classList.add("lookFilled-1H2Jvj"), e.classList.add("colorPrimary-2-Lusz"), e.classList.add("sizeSmall-3R2P2p"), e.classList.add("grow-2T4nbg"), e.classList.add("download-mj-all"), e.setAttribute("role", "link"), e.setAttribute("type", "button"), e.innerHTML = "<div class='contents-3NembX'><div class='content-1xP6ZE' aria-hidden='false'><div class='label-31sIdr'>Download All</div><b class='launchIcon-2KvOPN'>⏬</b></div></div>", t.appendChild(e) } } function downloadAllImages(t) { let e = t.parentElement.parentElement.parentElement.parentElement, a = e.querySelectorAll("a.originalLink-Azwuo9"); for (var n = 0; n < a.length; n++)downloadImage(a[n].href) } document.addEventListener("mouseover", e => { if (e.target.classList.contains("originalLink-Azwuo9")) { let t = e.target.parentElement; var a = e.target.href; if (!t.querySelector("a.download-mj")) { const n = document.createElement("a"); n.setAttribute("data-href", a), n.classList.add("download-mj"), n.style = "position: absolute;right: 10px;top: 10px;z-index: 9999;", n.text = "⬇️", t.appendChild(n) } } !e.target.classList.contains("container-2sjPya") || (e = e.target.querySelector("div.children-2XdE_I")) && initDownloadAllBtn(e) }), document.addEventListener("click", function (t) { t.target.classList.contains("download-mj") && (t.preventDefault(), downloadImage(t.target.getAttribute("data-href"))), t.target.classList.contains("label-31sIdr") && (t.preventDefault(), downloadAllImages(t.target.parentElement.parentElement.parentElement)), t.target.classList.contains("launchIcon-2KvOPN") && (t.preventDefault(), downloadAllImages(t.target.parentElement.parentElement.parentElement)) }); async function downloadImage(t) { const e = t.split("/").pop(); var a = e.split("?")[0]; const n = await fetch(t); t = await n.blob(), t = URL.createObjectURL(t); const l = document.createElement("a"); l.href = t, l.download = a, document.body.appendChild(l), l.click(), document.body.removeChild(l) }
+function initDownloadAllBtn(obe) {
+    if (!obe) return;
+    let pe = obe.parentElement, // children__*
+        obeHTML = obe.innerHTML;
+    let downloadAllBtn = document.createElement("button");
+    if (pe.querySelector("button.download-mj-all")) return;
+
+    obe.classList.forEach(function (className) {
+        downloadAllBtn.classList.add(className)
+    });
+
+    downloadAllBtn.classList.add("download-mj-all");
+    downloadAllBtn.setAttribute("role", "link");
+    downloadAllBtn.setAttribute("type", "button");
+
+    obeHTML = obeHTML.replace(/<div class="label([^"]+)"([^>]*)>(.*?)<\/div>/g, '<div class="download-all-label">Download All</div>');
+    downloadAllBtn.innerHTML = obeHTML.replace(/<svg[^>]*>.*?<\/svg>/, '<b class="' + obe.querySelector('svg').getAttribute('class') + '">⏬</b>');
+    pe.appendChild(downloadAllBtn)
+}
+
+function downloadAllImages(parentElement) {
+    let anchorTags = parentElement.querySelectorAll('a[data-role][data-safe-src]');
+    anchorTags.forEach(function (anchorTag) {
+        if (!anchorTag.href) return;
+        downloadImage(anchorTag.href)
+    });
+}
+document.addEventListener("mouseover", e => {
+    let parentLi = e.target.closest('li'),
+        parentElementRegex = /^messageListItem/;
+    if (!parentLi) return;
+    if (!parentElementRegex.test(parentLi.classList[0])) false;
+    let anchorTags = parentLi.querySelectorAll('a[data-role][data-safe-src]');
+    anchorTags.forEach(function (anchorTag) {
+        let pe = anchorTag.parentElement,
+            href = anchorTag.href;
+        if (!href || pe.querySelector("a.download-mj")) return;
+        let n = document.createElement("a");
+        n.setAttribute("data-href", href), n.classList.add("download-mj"), n.style = "position: absolute;right: 10px;top: 10px;z-index: 9999;", n.text = "⬇️", pe.appendChild(n)
+    });
+
+    let buttonElements = parentLi.querySelectorAll('button[role="link"][type="button"]');
+    if (!buttonElements) return;
+    initDownloadAllBtn(buttonElements[0])
+}), document.addEventListener("click", function (t) {
+    t.target.classList.contains("download-mj") && (t.preventDefault(), downloadImage(t.target.getAttribute("data-href")));
+    if (button = t.target.closest('button')){
+        if (button.classList.contains("download-mj-all")){
+            t.preventDefault(), downloadAllImages(button.closest('li'));
+        }
+    }
+    
+});
+async function downloadImage(t) {
+    const e = t.split("/").pop();
+    var a = e.split("?")[0];
+    const n = await fetch(t);
+    t = await n.blob(), t = URL.createObjectURL(t);
+    const l = document.createElement("a");
+    l.href = t, l.download = a, document.body.appendChild(l), l.click(), document.body.removeChild(l)
+}
